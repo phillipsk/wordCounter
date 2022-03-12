65e2b1fa8310ff84e0471b49bbb11e6fb03df7a2
phillipsk <inquiries@kevinphillips.email>
2022-03-12 15:54:19 -0500

    local working solution

--- src/main/kotlin/com/phillips/wordCounter/WordCounterApplication.kt
+++ src/main/kotlin/com/phillips/wordCounter/WordCounterApplication.kt
@@ -2,6 +2,12 @@ package com.phillips.wordCounter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class WordCounterApplication
@@ -9,3 +15,31 @@ class WordCounterApplication
 fun main(args: Array<String>) {
 	runApplication<WordCounterApplication>(*args)
 }
+
+@RestController
+@RequestMapping("/wordCount")
+class WordCountController {
+
+    private var wordMap = mutableMapOf<Int, WordEntity>()
+
+    @PostMapping
+    fun postWord(@RequestBody wordEntity: WordEntity): ResponseEntity<WordResponse> {
+        updateWordMap(wordEntity)
+        val totalWordCount: Int = wordMap.values.flatMap { it.message.split(" ".toRegex()) }.count()
+        return ResponseEntity(WordResponse(totalWordCount), HttpStatus.OK)
+    }
+
+    private fun updateWordMap(wordEntity: WordEntity) {
+        val mapCountNext: Int = if (wordMap.iterator().hasNext()) {
+            wordMap.maxOf { it.key }.plus(1)
+        } else {
+            1
+        }
+        if (!wordMap.values.any { it.id == wordEntity.id }) {
+            wordMap[mapCountNext] = wordEntity
+        }
+    }
+}
+
+data class WordEntity(val id: Int, var message: String)
+data class WordResponse(val count: Int)
